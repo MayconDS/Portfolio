6b8a659841defb4140f4a18873c78e44db2052a0
Maycon Douglas <crossfireelite601@gmail.com>
2022-11-01 20:54:51 -0300

    first commit

--- src/components/projects/CardProject.jsx
+++ src/components/projects/CardProject.jsx
@@ -9,15 +9,17 @@ const CardProject = ({ img, title, description }) => {
 
       <div className="links-project">
         <h1>{title}</h1>
-        <div className="demo button">
-          <a className="button-content" href="">
-            DEMO
-          </a>
-        </div>
-        <div className="github button">
-          <a className="button-content" href="">
-            GITHUB
-          </a>
+        <div className="btns">
+          <div className="demo button">
+            <a className="button-content" href="">
+              DEMO
+            </a>
+          </div>
+          <div className="github button">
+            <a className="button-content" href="">
+              GITHUB
+            </a>
+          </div>
         </div>
       </div>
     </div>
