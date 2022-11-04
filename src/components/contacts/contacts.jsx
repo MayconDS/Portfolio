1d30cc7f979dd1ccbb033f1c71357bcb0e25d666
Maycon Douglas <crossfireelite601@gmail.com>
2022-11-03 21:19:52 -0300

    fix bug effect reveal

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -54,81 +54,159 @@ const Contacts = () => {
         <div id="wave3" className="wave"></div>
         <div id="wave4" className="wave"></div>
       </div>
-      <Slide>
-        <h1 className="">CONTACTS</h1>´
-        <div className="container-contact">
-          <form onSubmit={(e) => sendEmail(e)}>
-            <input
-              type="text"
-              placeholder="Digite seu nome"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type="email"
-              name=""
-              id=""
-              placeholder="Digite seu E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <textarea
-              placeholder="Mensagem"
-              name=""
-              id=""
-              cols="40"
-              rows="5"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-            <input className="button-form" type="submit" value="Enviar" />
-          </form>
+      {window.innerWidth > 650 ? (
+        <Slide>
+          <h1 className="">CONTACTS</h1>´
+          <div className="container-contact">
+            <form onSubmit={(e) => sendEmail(e)}>
+              <input
+                type="text"
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Digite seu E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <textarea
+                placeholder="Mensagem"
+                name=""
+                id=""
+                cols="40"
+                rows="5"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              <input className="button-form" type="submit" value="Enviar" />
+            </form>
 
-          <div className="box-links">
-            <div className="email lnk">
-              <a href="mailto:maycondouglaszzlk@gmail.com">
-                <HiOutlineMail />
-              </a>
-              <span>E-MAIL</span>
-              <a href="mailto:maycondouglaszzlk@gmail.com">
-                maycondouglaszzlk@gmail.com
-              </a>
-            </div>
+            <div className="box-links">
+              <div className="email lnk">
+                <a href="mailto:maycondouglaszzlk@gmail.com">
+                  <HiOutlineMail />
+                </a>
+                <span>E-MAIL</span>
+                <a href="mailto:maycondouglaszzlk@gmail.com">
+                  maycondouglaszzlk@gmail.com
+                </a>
+              </div>
 
-            <div className="telephone lnk">
-              <a target="_blank" href="https://wa.me/+5584981916377">
-                <AiOutlinePhone />
-              </a>
-              <span>TELEFONE</span>
-              <a target="_blank" href="https://wa.me/+5584981916377">
-                (84) 98191-6377
-              </a>
+              <div className="telephone lnk">
+                <a target="_blank" href="https://wa.me/+5584981916377">
+                  <AiOutlinePhone />
+                </a>
+                <span>TELEFONE</span>
+                <a target="_blank" href="https://wa.me/+5584981916377">
+                  (84) 98191-6377
+                </a>
+              </div>
+              <div className="linkedin lnk">
+                <a href="https://www.linkedin.com/in/maycondouglasss/">
+                  <GrLinkedinOption />
+                </a>
+                <span>LINKEDIN</span>
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/maycondouglasss/"
+                >
+                  @maycondouglasss
+                </a>
+              </div>
+              <div className="github lnk">
+                <a target="_blank" href="https://github.com/MayconDS">
+                  <FiGithub />
+                </a>
+                <span>GITHUB</span>
+                <a target="_blank" href="https://github.com/MayconDS">
+                  @mayconds
+                </a>
+              </div>
             </div>
-            <div className="linkedin lnk">
-              <a href="https://www.linkedin.com/in/maycondouglasss/">
-                <GrLinkedinOption />
-              </a>
-              <span>LINKEDIN</span>
-              <a
-                target="_blank"
-                href="https://www.linkedin.com/in/maycondouglasss/"
-              >
-                @maycondouglasss
-              </a>
+          </div>{" "}
+          <span id="copy">© Maycon Douglas. All rights reserved.</span>
+        </Slide>
+      ) : (
+        <>
+          <h1 className="">CONTACTS</h1>´
+          <div className="container-contact">
+            <form onSubmit={(e) => sendEmail(e)}>
+              <input
+                type="text"
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Digite seu E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <textarea
+                placeholder="Mensagem"
+                name=""
+                id=""
+                cols="40"
+                rows="5"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              <input className="button-form" type="submit" value="Enviar" />
+            </form>
+
+            <div className="box-links">
+              <div className="email lnk">
+                <a href="mailto:maycondouglaszzlk@gmail.com">
+                  <HiOutlineMail />
+                </a>
+                <span>E-MAIL</span>
+                <a href="mailto:maycondouglaszzlk@gmail.com">
+                  maycondouglaszzlk@gmail.com
+                </a>
+              </div>
+
+              <div className="telephone lnk">
+                <a target="_blank" href="https://wa.me/+5584981916377">
+                  <AiOutlinePhone />
+                </a>
+                <span>TELEFONE</span>
+                <a target="_blank" href="https://wa.me/+5584981916377">
+                  (84) 98191-6377
+                </a>
+              </div>
+              <div className="linkedin lnk">
+                <a href="https://www.linkedin.com/in/maycondouglasss/">
+                  <GrLinkedinOption />
+                </a>
+                <span>LINKEDIN</span>
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/maycondouglasss/"
+                >
+                  @maycondouglasss
+                </a>
+              </div>
+              <div className="github lnk">
+                <a target="_blank" href="https://github.com/MayconDS">
+                  <FiGithub />
+                </a>
+                <span>GITHUB</span>
+                <a target="_blank" href="https://github.com/MayconDS">
+                  @mayconds
+                </a>
+              </div>
             </div>
-            <div className="github lnk">
-              <a target="_blank" href="https://github.com/MayconDS">
-                <FiGithub />
-              </a>
-              <span>GITHUB</span>
-              <a target="_blank" href="https://github.com/MayconDS">
-                @mayconds
-              </a>
-            </div>
-          </div>
-        </div>{" "}
-      </Slide>
-      <span id="copy">© Maycon Douglas. All rights reserved.</span>
+          </div>{" "}
+          <span id="copy">© Maycon Douglas. All rights reserved.</span>
+        </>
+      )}
     </footer>
   );
 };
