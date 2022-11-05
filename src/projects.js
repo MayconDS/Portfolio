c62244a94bddc08dfdb7e4bd5fb71c74ca355f74
Maycon Douglas <crossfireelite601@gmail.com>
2022-11-05 18:29:46 -0300

    repare css

--- src/projects.js
+++ src/projects.js
@@ -4,34 +4,29 @@ const projects = [
     title: 'WEATHERAPP',
     description:
       'WeatherApp app de previsões climáticas e informações meteorológicas',
-    id: 1,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/Cloneyt.png',
     title: 'CLONE YOUTUBE',
     description:
       'Clone da plataforma Youtube com a possibilidade de assistir videos',
-    id: 2,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/HBOMAX.png',
     title: 'CLONE HBOMAX',
     description:
       'Clone da plataforma HBOMAX buscando informações sobre filmes/series é sistema de login',
-    id: 3,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/GITHUB.png',
     title: 'GITHUB VIEWER',
     description:
       'GITHUB VIEWER visualizador de perfis do github trazendo os repositórios e informaçoes do perfil',
-    id: 4,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/TRAFALGAR.png',
     title: 'TRAFALGAR',
     description: 'LANDING PAGE desenvolvida atraves de um modelo do figma',
-    id: 5,
   },
   {
     img:
@@ -39,20 +34,17 @@ const projects = [
     title: 'JOGO DA VIDA',
     description:
       'JOGO DA VIDA representação do jogo criado pelo matematico John Conway',
-    id: 6,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/QUIZZ.png',
     title: 'QUIZ APP',
     description: 'QUIZ APP de perguntas relacionadas com programação',
-    id: 7,
   },
   {
     img: 'https://raw.githubusercontent.com/MayconDS/images/main/TODOLIST.png',
     title: 'TODO-LIST',
     description:
       'TODO-LIST desenvolvido para checagem de tarefas simples e importantes',
-    id: 8,
   },
 ]
 export default projects
