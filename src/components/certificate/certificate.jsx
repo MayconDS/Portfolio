c504fb70422175b6e902cfffbd1a87ed2a18840b
Maycon Douglas <crossfireelite601@gmail.com>
2022-11-05 21:07:25 -0300

    repare css

--- src/components/certificate/certificate.jsx
+++ src/components/certificate/certificate.jsx
@@ -2,42 +2,49 @@ import cfJavascript from "../../utils/imgs/cfJavascript.png";
 import cfAlgoritimo from "../../utils/imgs/cfAlgoritimo.png";
 import cfFrontend from "../../utils/imgs/cfFrontend.png";
 
+import { Slide } from "react-awesome-reveal";
+
 import "./certificate.css";
 
 const Certificate = () => {
   return (
     <section id="certificate" className="certificate">
       <div className="container">
-        <h1>CERTIFICADOS</h1>
-        <div className="grid-3">
-          <div className="card-certificate">
-            <img src={cfJavascript} alt="" />
-            <a
-              target="_blank"
-              href="https://www.coursera.org/account/accomplishments/certificate/U5XWJAVSUD9G"
-            >
-              VERIFICAR
-            </a>
+        <Slide direction="up">
+          {" "}
+          <h1>CERTIFICADOS</h1>
+        </Slide>
+        <Slide direction="up">
+          <div className="grid-3">
+            <div className="card-certificate">
+              <img src={cfJavascript} alt="" />
+              <a
+                target="_blank"
+                href="https://www.coursera.org/account/accomplishments/certificate/U5XWJAVSUD9G"
+              >
+                VERIFICAR
+              </a>
+            </div>
+            <div className="card-certificate">
+              <img src={cfAlgoritimo} alt="" />
+              <a
+                target="_blank"
+                href="https://www.freecodecamp.org/certification/maycondouglas/javascript-algorithms-and-data-structures"
+              >
+                VERIFICAR
+              </a>
+            </div>
+            <div className="card-certificate">
+              <img src={cfFrontend} alt="" />
+              <a
+                target="_blank"
+                href="https://www.coursera.org/account/accomplishments/certificate/558MJF7EFXPY"
+              >
+                VERIFICAR
+              </a>
+            </div>
           </div>
-          <div className="card-certificate">
-            <img src={cfAlgoritimo} alt="" />
-            <a
-              target="_blank"
-              href="https://www.freecodecamp.org/certification/maycondouglas/javascript-algorithms-and-data-structures"
-            >
-              VERIFICAR
-            </a>
-          </div>
-          <div className="card-certificate">
-            <img src={cfFrontend} alt="" />
-            <a
-              target="_blank"
-              href="https://www.coursera.org/account/accomplishments/certificate/558MJF7EFXPY"
-            >
-              VERIFICAR
-            </a>
-          </div>
-        </div>
+        </Slide>
       </div>
     </section>
   );
